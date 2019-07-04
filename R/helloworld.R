becb4aabe9726b46c580187ac2d1cf2f5e9c852b
January Weiner <january.weiner@gmail.com>
2019-07-04 11:57:28 +0200

    update project

---
+++ R/helloworld.R
@@ -0,0 +1,23 @@
+#' Say Hello
+#'
+#' Say Hello, but only to Berlin
+#'
+#' Sucks if you are not Berlin.
+#' @param message What do you want to say to Berlin?
+#' @examples
+#' helloBerlin("How are you?")
+#' @export
+helloBerlin <- function(message) {
+
+  cat("Hello, Berlin!\nA special message for you:\n")
+  cat(message)
+  cat("\n")
+}
+
+#' Say good bye
+#'
+#' @export
+goodByeBerlin <- function() {
+  cat("Goodbye – Guten Einkauf\n")
+}
+
